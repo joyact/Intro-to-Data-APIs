390e421c5f4b1536196598126c7f1ba9ffa621eb
Joy Lee <joylee.dev@gmail.com>
2021-02-15 23:13:07 +0900

    Data Selfie App : database by NeDB

--- module2/index.js
+++ module2/index.js
@@ -1,35 +1,31 @@
-// Node module Express : create a web server
+const express = require('express'); // create a web server
+const Datastore = require('nedb'); // create a database
 
-const express = require('express'); // import express
 const app = express(); // excute express and put in a variable
 
-// listen at a specific port
-app.listen(3000, () => console.log('listening at 3000'));
+app.listen(3000, () => console.log('listening at 3000')); // listen at a specific port
+app.use(express.static('public')); // let the server host static content (publicly accessible files)
+app.use(express.json({ limit: '1mb' })); // let the server parse any incoming data as JSON
 
-// let the server host static content
-// anything in this directory is publicly accessible from URL
-app.use(express.static('public'));
-
-// let the server parse any incoming data as JSON
-app.use(express.json({ limit: '1mb' }));
-
-const database = [];
+const database = new Datastore('database.db'); // new database
+database.loadDatabase(); // create a database when the server is ran
 
 // set up the endpoint for my route called 'api'
 app.post('/api', (request, response) => {
   // receive data request from the client
   console.log('I got a request!');
-  console.log(request.body);
+  const data = request.body;
 
   // every time the server receives new data, push it to database
-  const data = request.body;
-  database.push(data);
-  console.log(database);
+  const timestamp = Date.now();
+  data.timestamp = timestamp;
+  database.insert(data); // insert data to database
 
   // send back to client
   response.json({
     status: 'success',
-    latitude: request.body.lat,
-    longitudt: request.body.lon,
+    timestamp: timestamp,
+    latitude: data.lat,
+    longitudt: data.lon,
   });
 });
