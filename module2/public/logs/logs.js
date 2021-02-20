0cb5d5bacf0c59e7590c1de4f5bfa2b3ca9a26e4
Joy Lee <joylee.dev@gmail.com>
2021-02-20 18:37:20 +0900

    Data Selfie App : sort by time, mood

--- module2/public/logs/logs.js
+++ module2/public/logs/logs.js
@@ -1,4 +1,23 @@
 const Container = document.getElementById('container');
+const sortByTime = document.getElementById('time');
+const sortByMood = document.getElementById('mood');
+
+let selfies = [];
+
+sortByTime.addEventListener('click', () => {
+  sortData((a, b) => b.time - a.time);
+});
+
+sortByMood.addEventListener('click', () => {
+  sortData((a, b) => (a.mood > b.mood ? 1 : -1));
+});
+
+function sortData(compare) {
+  selfies.sort(compare);
+  for (let item of selfies) {
+    Container.append(item.list);
+  }
+}
 
 async function getData() {
   const response = await fetch('/api');
@@ -10,15 +29,18 @@ async function getData() {
     const date = document.createElement('p');
     const mood = document.createElement('p');
     const image = document.createElement('img');
-    const dateString = new Date(item.timestamp).toLocaleDateString();
+    const dateString = new Date(item.timestamp).toLocaleString();
 
     geo.textContent = `lat : ${item.lat}°, lon : ${item.lon}°`;
     date.textContent = dateString;
     mood.textContent = `mood: ${item.mood}`;
     image.src = item.image64;
+    image.alt = "today's mood";
 
     list.append(geo, date, mood, image);
+    selfies.push({ list: list, time: item.timestamp, mood: item.mood });
     Container.append(list);
   }
 }
+
 getData();
